b5902505abb0bdb51ecddb6b442065f061e3255f
maxlukyrabbit <maxlukyrabbit@yandex.ru>
2025-01-06 10:26:40 +0300

    Button STK

--- My_Application/app/src/main/java/com/example/my_application/STK_main.java
+++ My_Application/app/src/main/java/com/example/my_application/STK_main.java
@@ -1,8 +1,6 @@
 package com.example.my_application;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class menu_selection extends AppCompatActivity {
+public class STK_main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu_selection);
+        setContentView(R.layout.activity_stk_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-    public void STK(View v){
-        Intent intent = new Intent(menu_selection.this, STK_main.class);
-        startActivity(intent);
-    }
-
-    public void SK_Warranty(View v){
-        Intent intent = new Intent(menu_selection.this, MainActivity.class);
-        startActivity(intent);
-    }
-
 }
